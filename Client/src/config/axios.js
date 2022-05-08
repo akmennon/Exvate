ad84d11ef00c538ca23fa74852e88ce735360a45
Ajay <kajaymenon@gmail.com>
2022-05-08 20:53:56 +0530

    Split jwt to cookie and jwt to be stored in local storage

--- Client/src/config/axios.js
+++ Client/src/config/axios.js
@@ -2,7 +2,8 @@ import Axios from 'axios'
 
 const axios = Axios.create({
     baseURL:'http://localhost:3015',
-    timeout:20000
+    timeout:20000,
+    withCredentials:true
 })
 
 axios.interceptors.response.use(function(response){
